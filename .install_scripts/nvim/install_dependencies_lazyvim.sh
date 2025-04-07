1dffa265c35b33cc7fbaeafb0b57b058de03b710
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-04-07 18:50:58 -0300

    implement install_dependencies_lazyvim

---
+++ .install_scripts/nvim/install_dependencies_lazyvim.sh
@@ -0,0 +1,22 @@
+#! usr/bin/bash
+
+# lazygit
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+# fzf-lua
+sudo apt install fzf ripgrep fd-find
+
+# mason
+
+sudo apt unzip
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+node -v 
+nvm current 
+npm -v 
