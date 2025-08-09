567b790af99628f33fac66c613635c585c33cdb6
Gabriel Romao <gabriel-rgomes@hotmail.com>
2025-08-09 14:42:26 -0300

    add install ghostty script

---
+++ .install_scripts/ghostty/build_ghostty.sh
@@ -0,0 +1,7 @@
+sudo apt upgrade
+
+yes Y | sudo apt install libgtk-4-dev libadwaita-1-dev
+
+# snap install zig --classic --beta
+
+sudo snap install ghostty --classic
--- .install_scripts/nvim/build_neovim.sh
+++ .install_scripts/nvim/build_neovim.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-yes Y | sudo apt-get install cmake ninja-build gettext curl build-essential 
+sudo apt upgrade
+
+yes Y | sudo apt install cmake ninja-build gettext curl build-essential
 git clone https://github.com/neovim/neovim $HOME/personal/neovim
 
 cd $HOME/personal/neovim
@@ -8,4 +10,3 @@ git checkout stable
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-
